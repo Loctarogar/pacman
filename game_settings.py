2ee444940939fd7b79346219eb0e2377eebcb46a
Loctarogar <igrsl1990@gmail.com>
2020-03-23 19:02:31 +0100

    settings and base game class

---
+++ game_settings.py
@@ -0,0 +1,9 @@
+# screen settings
+WIDTH = 800
+HEIGHT = 600
+
+# define colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
---
+++ main.py
@@ -0,0 +1,30 @@
+import pygame as pg
+from os import *
+from game_settings import *
+
+class Game:
+    def __init__(self):
+        pg.init()
+        self.running = True
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        self.clock = pg.time.Clock()
+        
+    def new_game(self):
+        self.run()
+                
+    def run(self):
+        # events check
+        self.clock.tick(60)
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.running = False
+                
+        self.screen.fill(BLACK)
+        pg.display.flip()
+      
+game = Game()          
+while game.running:
+    game.new_game()
+        
+    
+pg.quit()
