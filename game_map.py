a47ff8faa05716fc484eb8f6fc8c94a20c5599d3
Loctarogar <igrsl1990@gmail.com>
2020-03-23 20:40:48 +0100

    TODO: draw map

--- game_map.py
+++ game_map.py
@@ -1,11 +1,14 @@
 import pygame as pg
-from settings import *
+from game_settings import *
 
 class Map(pg.sprite.Sprite):
-    def __init__(self, game):
+    def __init__(self):
         pg.sprite.Sprite.__init__(self)
-        self.game = game
+        # list of rects for map
+        map_list = [
+                 (350, 200, 100, 20),
+                 (175, 100, 50, 20),
+                 ]
         self.image = pg.Surface((10, 50))
         self.image.fill(WHITE)
         self.rect = self.image.get_rect()
-        
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pygame as pg
 from os import *
 from game_settings import *
+from game_map import *
 
 class Game:
     def __init__(self):
         pg.init()
+        self.map = Map()
         self.running = True
         self.screen = pg.display.set_mode((WIDTH, HEIGHT))
         self.clock = pg.time.Clock()
@@ -20,14 +22,14 @@ class Game:
                 self.running = False
                 
         self.screen.fill(BLACK)
+        self.draw_map()
         pg.display.flip()
         
     def draw_map(self):
-        pg.draw.rect()
+        pg.draw.rect(self.screen, WHITE, (400, 400, 10, 20))
       
 game = Game()          
 while game.running:
     game.new_game()
         
-    
 pg.quit()
