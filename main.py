19ccbd7673f5c222c151d6025859efa2dcb2ea57
Loctarogar <igrsl1990@gmail.com>
2020-03-24 21:01:32 +0100

    base map

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ class Game:
         self.running = True
         self.screen = pg.display.set_mode((WIDTH, HEIGHT))
         self.clock = pg.time.Clock()
-        
+
     def new_game(self):
         self.run()
-                
+
     def run(self):
         # events check
         self.clock.tick(60)
@@ -26,9 +26,9 @@ class Game:
     def draw_map(self):
         for i in MAP_LIST:
             pg.draw.rect(self.screen, WHITE, (i))
-      
+
 game = Game()          
 while game.running:
     game.new_game()
-        
+
 pg.quit()
