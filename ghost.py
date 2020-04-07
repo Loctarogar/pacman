9f145764988e2e6ec0f2577e2506cb41536063af
Loctarogar <igrsl1990@gmail.com>
2020-04-07 18:59:36 +0200

    ghost success

--- ghost.py
+++ ghost.py
@@ -14,45 +14,58 @@ class Ghost(pg.sprite.Sprite):
         self.directions = []
         
     def change_direction(self):
-        self.directions.clear()
-        self.directions = [UP, DOWN, LEFT, RIGHT]
-        self.directions.remove(self.direction)
+        # self.directions = [UP, DOWN, LEFT, RIGHT]
+        #self.directions.remove(self.direction)
         self.direction = self.directions[random.randint(0, int(len(self.directions) - 1))]
         
-
     def check_change_direction(self):
         self.directions.clear()
         
         # check direction up
-        self.rect.y -= 5
+        self.rect.y -= 20
         hits = pg.sprite.spritecollide(self.game.ghost, self.game.map_sprite, False)
-        if hits:
+        if not hits:
             self.directions.append(UP)
-        self.rect.y += 5
+        self.rect.y += 20
         
         # check direction down
-        self.rect.y += 5
+        self.rect.y += 20
         hits = pg.sprite.spritecollide(self.game.ghost, self.game.map_sprite, False)
-        if hits:
+        if not hits:
             self.directions.append(DOWN)
-        self.rect.y -= 5
+        self.rect.y -= 20
         
         # check direction left
-        self.rect.x -= 5
+        self.rect.x -= 20
         hits = pg.sprite.spritecollide(self.game.ghost, self.game.map_sprite, False)
-        if hits:
+        if not hits:
             self.directions.append(LEFT)
-        self.rect.y += 5
+        self.rect.x += 20
         
         # check direction right
-        self.rect.x += 5
+        self.rect.x += 20
         hits = pg.sprite.spritecollide(self.game.ghost, self.game.map_sprite, False)
-        if hits:
-            self.directions.append(DOWN)
-        self.rect.x -= 5
+        if not hits:
+            self.directions.append(RIGHT)
+        self.rect.x -= 20
+        print (self.directions)
+        print (len(self.directions))
+        if len(self.directions) > 2:
+            return True
+        else:
+            return False
 
     def move(self):
-        #self.check_change_direction()
+        # get possible directions
+        print (self.check_change_direction())
+        if self.check_change_direction():
+            # should change directions
+            coin = random.randint(0, 1)
+            if coin:
+                self.change_direction()
+        
+        
+        print (self.direction)
         if self.direction == UP:
             self.rect.y -= 1
             hits = pg.sprite.spritecollide(self.game.ghost, self.game.map_sprite, False)
